a7de34ee731bff133ab619eb49e0cc408915e311
손재건 <snasopp@gmail.com>
2026-09-18 11:06:54 +0900

    Create ch04-3.c

---
+++ ch04/ch04-3.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#include <stdio.h>
+
+int main(void)
+{
+    char str[100];
+    int i = 0;
+
+    printf("문자열을 입력하시오:");
+    scanf("%s", str);
+
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] += 32;
+        }
+        else if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] -= 32;
+        }
+
+        i++;
+    }
+
+    printf("변환결과 : %s\n", str);
+
+    return 0;
+}
